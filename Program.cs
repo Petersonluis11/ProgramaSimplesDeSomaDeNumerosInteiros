e982b546b42b9bcbe1cd687bd303e59668f36167
Peterson <petersonluisalves.pla@gmail.com>
2021-09-20 19:12:41 -0300

    Soma

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1, b2, soma;
+            
+            Console.WriteLine("Seja bem vindo");
+    
+
+            Console.WriteLine("Digite um numero:  ");
+            a1 = int.Parse(Console.ReadLine());  
+
+             Console.WriteLine("Digite um numero:  ");
+            b2 = int.Parse(Console.ReadLine());
+
+             soma = a1 + b2;
+
+             Console.WriteLine(" Soma:" +soma);  
+
+            Console.ReadKey();     
+        }
+    }
+}
